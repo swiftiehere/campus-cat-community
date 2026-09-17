f7b05f161459b6576ee5f8d66b194538b972f5c8
swiftiehere <hqxu@eitech.edu.cn>
2026-09-17 22:44:59 +0800

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,5 @@
+// Optional cloud mode. Fill these values after creating a free Supabase project.
+window.CAT_ARCHIVE_CONFIG = {
+  url: 'https://tjdpnmsrgwrusdrzstll.supabase.co/rest/v1/',
+  anonKey: 'sb_publishable_NWaK4zWR-NCmMuCFdtz59A_jpkOWkld'
+};
